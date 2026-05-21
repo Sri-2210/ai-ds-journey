9fa6cac543b023affa6c20ea12898e908f7197eb
Sri-2210 <keerthikommuru22@gmail.com>
2026-05-21 23:37:41 +0530

    Update hackerrank.py

--- week1-python-eda/day01-setup-basics/hackerrank.py
+++ week1-python-eda/day01-setup-basics/hackerrank.py
@@ -1,1 +1,33 @@
+#Problem 1 - python if else (
 
+if n%2!=0:
+    print("Weird")
+else:
+    if(n>=2 and n<=5):
+        print("Not Weird")
+    elif(n>=6 and n<=20):
+        print('Weird')
+    else:
+        print("Not Weird")  
+
+#-----------------------------------------------------
+#-----------------------------------------------------
+
+#Problem 2 - python arthimetic operators 
+
+a = int(input())
+b = int(input())
+print(a+b)
+print(a-b)
+print(a*b)
+
+
+#-----------------------------------------------------
+#-----------------------------------------------------
+
+#Problem 3 - python division 
+
+    a = int(input())
+    b = int(input())
+    print(a//b)
+    print(a/b)
